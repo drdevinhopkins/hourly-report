3555e48d603e25da119cda8ed61a2ab6172595b6
Devin Hopkins <devinhopkins@gmail.com>
2022-01-17 12:24:19 -0500

    blah

--- scripts/daily-visits-update.py
+++ scripts/daily-visits-update.py
@@ -1,5 +1,20 @@
 import pandas as pd
+import datetime
 
-df = pd.read_csv("data/daily-visits.csv")
+df = pd.read_csv('data/daily-visits.csv')
+df.ds = pd.to_datetime(df.ds)
 
-df2 = pd.read_csv("data/recent.csv")
+archive = pd.read_csv('../data/since-2020.csv')
+archive.ds = pd.to_datetime(archive.ds)
+archive = archive.set_index('ds')
+
+archive = archive[archive.index.hour == 0][['Total Inflow cum']]
+archive.columns = ['y']
+archive = archive.reset_index()
+
+archive['ds'] = archive['ds'] - datetime.timedelta(days=1)
+
+df = pd.concat([df, archive], ignore_index=True).drop_duplicates(
+    keep='first').sort_values(by='ds', ascending=False).reset_index(drop=True)
+
+df.to_csv('data/daily-visits.csv', index=False)
