c4b8da2ef1edc7e42f2807387a19da7685fbb322
drdevinhopkins <41838748+drdevinhopkins@users.noreply.github.com>
2022-06-12 15:11:38 -0400

    Update updateWeather.py

--- scripts/updateWeather.py
+++ scripts/updateWeather.py
@@ -1,4 +1,14 @@
-from datetime import date
 import pandas as pd
 
-old_weather = pd.read_csv('data/archiveAndForecast.csv')
+old_weather = pd.read_csv('data/weatherArchiveAndForecast.csv')
+old_weather.ds = pd.to_datetime(old_weather.ds)
+
+forecast = pd.read_csv(
+    'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/montreal/next24hours?unitGroup=metric&include=hours&key=NDD4FLZ37NRPKGD82DRZWXGHU&contentType=csv')
+forecast = forecast.rename(columns={'datetime': 'ds'})
+forecast.ds = pd.to_datetime(forecast.ds)
+
+new_weather = pd.concat(
+    [old_weather, forecast], join='inner').drop_duplicates(subset='ds', keep='last')
+
+new_weather.to_csv('data/weatherArchiveAndForecast.csv', index=False)
