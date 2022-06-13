b9938457217105a0a143478c3ea1ce55e62e229e
drdevinhopkins <41838748+drdevinhopkins@users.noreply.github.com>
2022-06-13 15:22:54 +0000

    fix

--- scripts/updateWeather.py
+++ scripts/updateWeather.py
@@ -12,3 +12,19 @@ new_weather = pd.concat(
     [old_weather, forecast], join='inner').drop_duplicates(subset='ds', keep='last')
 
 new_weather.to_csv('data/weatherArchiveAndForecast.csv', index=False)
+
+
+old_weather = pd.read_csv('data/daily-weather.csv')
+old_weather.ds = pd.to_datetime(old_weather.ds)
+
+forecast = pd.read_csv(
+    'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/montreal?unitGroup=metric&include=days&key=RGEY54DWN2UNQWC86N2UVF4CU&contentType=csv')
+forecast = forecast.rename(columns={'datetime': 'ds'})
+forecast.ds = pd.to_datetime(forecast.ds)
+
+new_weather = pd.concat(
+    [old_weather, forecast], join='inner').drop_duplicates(subset='ds', keep='last')
+
+new_weather.to_csv('data/daily-weather.csv', index=False)
+
+
