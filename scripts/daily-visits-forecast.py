30e2f0e79daa248611553b7ca0e1ecd15e8a1868
drdevinhopkins <41838748+drdevinhopkins@users.noreply.github.com>
2023-06-06 10:52:24 -0400

    Update daily-visits-forecast.py

--- scripts/daily-visits-forecast.py
+++ scripts/daily-visits-forecast.py
@@ -3,6 +3,16 @@ import pandas as pd
 from prophet import Prophet
 import datetime
 
+import os
+from deta import Deta
+from dotenv import load_dotenv
+
+load_dotenv()
+
+deta = Deta(os.environ.get("DETA_PROJECT_KEY"))
+
+forecasts = deta.Drive("forecasts")
+
 df = pd.read_csv('data/daily-visits.csv')
 df.ds = pd.to_datetime(df.ds)
 df = df[df.ds >= '2020-02-01']
@@ -20,3 +30,6 @@ forecast = m.predict(future)
 forecast = forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']]
 
 forecast.to_csv('data/daily-visits-forecast.csv', index=False)
+
+for file in ['daily-visits.csv', 'daily-visits-forecast.csv']:
+    forecasts.put(file, path='data/'+file)
